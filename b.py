c0b2b7727901df4c98b01972bf4babf1dac13e1f
empalamado <empalamado@gmail.com>
2018-08-31 18:09:29 -0300

    Update b.py

--- b.py
+++ b.py
@@ -1,2 +1,16 @@
 import c     # File b.py
 Y = 222
+
+#Pergunta: O que acontece se eu atribuir um número inteiro a uma variável flutuante?
+#Resposta: Podemos investigar esse comportamento criando uma nova variável.
+
+y = 1.0
+
+#Esta declaração cria uma variável chamada y que contém o valor inteiro 1. 
+#Mas y é uma variável inteira ou de ponto flutuante? Podemos descobrir vendo o conteúdo de y. Python imprimiu o valor com uma parte 
+#fracionária (que é zero). Isso indica que o valor é um valor de ponto flutuante. 
+#Em outras palavras, A PRESENÇA DE UM PONTO DECIMAL EM UM VALOR IMPRESSO NOS DIZ QUE O VALOR É UM FLOAT. 
+#O valor é sempre ponto flutuante quando um decimal é incluído, mesmo se não houver casas decimais.
+
+#Pergunta: E quanto aos cálculos de ponto flutuante e inteiro?
+#Resposta: Podemos investigar os resultados dos cálculos digitando alguns e depois observando os resultados.
