77b2a79366eb32fc6cb93a51714f99cbd28d5e64
empalamado <empalamado@gmail.com>
2018-08-31 23:08:36 -0300

    Update b.py

--- b.py
+++ b.py
@@ -24,4 +24,23 @@ import c
  # Acontece que quando uma expressão contém um valor de ponto flutuante, o resultado do cálculo é automaticamente convertido 
  # em um valor de ponto flutuante. Geralmente, se a expressão contiver números inteiros, gerará um resultado inteiro. 
  # Se a expressão contiver um valor de ponto flutuante, a expressão gerará um resultado de ponto flutuante.
- # Uma exceção à regra “inteiros fazem inteiros” é quando dividimos um inteiro por outro
+ # Uma exceção à regra “inteiros fazem inteiros” é quando dividimos um inteiro por outro.
+  
+>>> 1/2     #O valor de 1/2 é uma metade, que não pode ser retido em um inteiro, portanto, o resultado desse cálculo é zero
+0.5         #no Python 2.7. Na verdade, qualquer número menor que 1 é truncado para zero nesse cálculo.
+            #Isso significa que o resultado de 9/10, que deveria ser 0,9, também é zero. Isso pode resultar em somas muito 
+#diferentes dos valores que podemos esperar.
+#Isso levanta a terrível perspectiva de programas em Python que funcionam corretamente no Python 3.6, produzindo respostas 
+#completamente erradas quando eles são executados em versões mais antigas do Python. 
+#O melhor conselho que posso dar é sempre colocar um ponto decimal em um valor se você quiser gerar resultados de ponto flutuante
+#quando usado em cálculos.
+#Você pode fazer com que um programa do Python 2 se comporte da mesma maneira que o Python 3, dizendo-lhe para usar as rotinas 
+#de divisão atualizadas: Colocando o código abaixo:
+
+from __future__ import division
+
+#Você deve dar este comando no início de um programa para fazer a divisão do Python 2 se comportar
+#da mesma maneira que a divisão do Python 3.
+
+
+
